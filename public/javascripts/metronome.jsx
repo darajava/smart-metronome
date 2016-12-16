da3f2af2a6f338058a99ba95c0df98cf9f28ef73
Dara Javaherian <darajavaherian@gmail.com>
2016-12-16 18:01:41 +0000

    Cleanup

--- public/javascripts/metronome.jsx
+++ public/javascripts/metronome.jsx
@@ -1,22 +1,3 @@
-function playSound(soundObj) {
-    var frequency = Math.floor(Math.random() * 500) + 50;
-    var mainOscillator = context.createOscillator();
-    var subOscillator = context.createOscillator();
-
-    mainOscillator.type = "square";
-    mainOscillator.frequency.value = frequency;
-    mainOscillator.connect(context.destination);
-    subOscillator.type = "square";
-    subOscillator.frequency.value = frequency;
-    subOscillator.detune.value = -1200;
-    subOscillator.connect(context.destination);
-
-    mainOscillator.start(context.currentTime);
-    mainOscillator.stop(context.currentTime + 2);   
-    subOscillator.start(context.currentTime + 1);
-    subOscillator.stop(context.currentTime + 2);
-}
-
 var Metronome = React.createClass({
   render: function() {
     return (
@@ -28,17 +9,7 @@ var Metronome = React.createClass({
 class StartButton extends React.Component {
   constructor(props) {
     super(props);
-    this.timer = new Tock({
-      countdown: false,
-      interval: 1000 * 60 / 200,
-      callback: this.metronomeTick
-    });
-    this.count = 0;
-    
-    this.lastCount = 0;
-    this.metronomeTick = this.metronomeTick.bind(this)
     this.startMetronome = this.startMetronome.bind(this)
-  
   }
 
   startMetronome(e) {
@@ -53,7 +24,7 @@ class StartButton extends React.Component {
     _gain.connect(context.destination);
   
     var beats = 160;
-    var bpm = 144;
+    var bpm = 60;
     var beat = 1/100;
     var rest = (60 / bpm) - beat;
     var t = context.currentTime;
@@ -77,17 +48,6 @@ class StartButton extends React.Component {
       _gain.gain.setValueAtTime(gainVal, t);
     }
     oscillator.start(0);
-  }
-
-  metronomeTick() {
-    playSound('beat1');
-    console.log( 'tick' );
-    ReactDOM.render(
-      <Metronome message={this.lastCount - Date.now()}/>,
-      document.getElementById('root')
-    );
-    this.lastCount = Date.now();
-    this.count++;
   }
 
   stopMetronome() {
