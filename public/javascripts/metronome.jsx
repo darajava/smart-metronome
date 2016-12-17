a5dcb552878e79a4f5ebdec59b80f89f476ef223
Dara Javaherian <darajavaherian@gmail.com>
2016-12-17 15:43:03 +0000

    More reactful and implement stop button

--- public/javascripts/metronome.jsx
+++ public/javascripts/metronome.jsx
@@ -1,64 +1,77 @@
-var Metronome = React.createClass({
-  render: function() {
-    return (
-      <p>{this.props.message}</p>
-    );
-  }
-});
-
-class StartButton extends React.Component {
+class Metronome extends React.Component {
   constructor(props) {
     super(props);
-    this.startMetronome = this.startMetronome.bind(this)
+    this.context = new (window.AudioContext || window.webkitAudioContext());
+    this.oscillator = this.context.createOscillator();
+    this._gain = this.context.createGain();
+    this.oscillator.frequency.value = 550;
+    this._gain.gain.value = 0;
+    this._gain.connect(this.context.destination);
+    this.oscillator.start(0);
+    
+    this.t = this.context.currentTime;
+    
+    this.startMetronome = this.startMetronome.bind(this);
+    this.stopMetronome = this.stopMetronome.bind(this);
+    
+    this.state = {
+      count: 0,
+      counting:  false
+    }
   }
-
+  
   startMetronome(e) {
-    var context = new (window.AudioContext || window.webkitAudioContext());
-    var oscillator = context.createOscillator();
-    var _gain = context.createGain();
-
-    oscillator.frequency.value = 220;
-    oscillator.connect(_gain);
-
-    _gain.gain.value = 0;
-    _gain.connect(context.destination);
-  
     var beats = 160;
-    var bpm = 60;
-    var beat = 1/100;
+    var bpm = 70;
+    var beat = 1/200;
     var rest = (60 / bpm) - beat;
-    var t = context.currentTime;
-  
-    for (var i = 0; i < beats; i++) {
+
+    for (var i = -1; i < beats; i++) {
       if (i % 4 == 0) {
-        console.log('beat');
-        oscillator.frequency.value = 220;
-       
-        oscillator.type = 'square';
         var gainVal = 1.0;
       } else {
-        console.log('bar');
-        oscillator.frequency.value = 440;
-        oscillator.type = 'triangle';
         gainVal = 0.5;
       }
-      t += beat;
-      _gain.gain.setValueAtTime(0.0, t);
-      t += rest;
-      _gain.gain.setValueAtTime(gainVal, t);
+      this.t += beat;
+      this._gain.gain.setValueAtTime(0.0, this.t);
+      this.t += rest;
+      this._gain.gain.setValueAtTime(gainVal, this.t);
     }
-    oscillator.start(0);
+    this._gain.gain.setValueAtTime(0.0, this.t);
+    this.oscillator.connect(this._gain);
+
+    this.setState({counting: true});
   }
 
   stopMetronome() {
-    ReactDOM.render(<StartButton start='Start'/>, document.getElementById('button'));
+    this.oscillator.disconnect(this._gain);
+    this.setState({counting: false});
   }
 
   render() {
     return (
-      <button onClick={this.props.start == 'Start' ? this.startMetronome : this.stopMetronome}>{this.props.start}</button>
+      <div>
+        <p>{this.count}</p>
+        <StartButton
+          onClick={() => this.state.counting ? this.stopMetronome() : this.startMetronome()}
+          counting={this.state.counting} />
+      </div>
     );
   }
 }
 
-ReactDOM.render(<StartButton start='Start'/>, document.getElementById('button'));
+class StartButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button onClick={() => this.props.onClick()}>
+        {this.props.counting ? 'Stop' : 'Start'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<Metronome/>, document.getElementById('root'));
