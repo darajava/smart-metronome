97887b4c6034a0d97ffbf5109e3506fa2fa98a83
Dara Javaherian <darajavaherian@gmail.com>
2016-12-19 16:07:41 +0000

    Give information on the scale you are playing

--- public/javascripts/metronome.jsx
+++ public/javascripts/metronome.jsx
@@ -1,3 +1,30 @@
+String.prototype.ucFirst = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
+Number.prototype.toEnglish = function() {
+  if (this == '1')
+    return 'one';
+  if (this == '2')
+    return 'two';
+  if (this == '3')
+    return 'three';
+  if (this == '4')
+    return 'four';
+  if (this == '5')
+    return 'five';
+  if (this == '6')
+    return 'six';
+  if (this == '7')
+    return 'seven';
+  if (this == '8')
+    return 'eight';
+  if (this == '9')
+    return 'nine';
+  if (this == '10')
+    return 'ten';
+  return this;
+}
+
 class Metronome extends React.Component {
   constructor(props) {
     super(props);
@@ -11,14 +38,16 @@ class Metronome extends React.Component {
     this._gain.gain.value = 0;
     this._gain.connect(this.ac.destination);
     this.oscillator.start(0);
-   
+  
+    this.hands = "both"; 
     this.beatsInBar = 4;
     this.reps = 6; 
-    var octaves = 1;
+    this.octaves = 1;
     var degreesOfScale = 7;
-    var totalNotesInScale = degreesOfScale * octaves * 2 + 1
-    this.notesInScale = Math.ceil(totalNotesInScale / 4) * 4;
+    var totalNotesInScale = degreesOfScale * this.octaves * 2 + 1;
 
+    this.notesPerBeat = 4;
+    this.beatsInScale = (Math.ceil(totalNotesInScale / 4) * 4) / this.notesPerBeat;
  
     this.state = {
       count: 0,
@@ -48,7 +77,7 @@ class Metronome extends React.Component {
     beat = 1/200;
     rest = (60 / bpm) - beat;
     for (var rep = 0; rep < this.reps; rep++){
-      for (var i = 1; i <= this.notesInScale; i++) {
+      for (var i = 1; i <= this.beatsInScale; i++) {
         if ((i-1) % 4 == 0) {
           gainVal = 1.0;
         } else {
@@ -83,14 +112,29 @@ class Metronome extends React.Component {
     var progresses = [];
     for (var i=1; i <= 6; i++) {
       progresses.push(
-        <Progress key={i} complete={this.state.count > i * this.notesInScale}/>
+        <Progress key={i} number={i} complete={this.state.count > i * this.beatsInScale}/>
       );
     }
 
     return (
       <div>
-        <div>{(this.state.count - 1) % this.beatsInBar + 1}</div>
+        <div className="todo">
+          <h2>Play <b>{this.props.scale.displayName}</b>:</h2> 
+          <div className="image-holder">
+            <img className="piano-scale" src={"/images/scales/" + this.props.scale.name + ".png"} />
+          </div>
+          <ul className="rounded-list">
+            <li><b>{this.reps.toEnglish().ucFirst()}</b> time{this.reps == 1 ? '' : 's'}</li>
+            <li><b>{this.notesPerBeat.toEnglish().ucFirst()}</b> notes per beat</li>
+            <li><b>{this.octaves.toEnglish().ucFirst()}</b> Octave{this.octaves == 1 ? '' : 's'}</li>
+            <li><b>Similar</b> motion</li>
+            <li><b>{this.hands.ucFirst()}</b> hands</li>
+            <li>BPM: <b>{this.state.bpm}</b></li>
+            <li>Adjusted BPM: <b>{this.state.bpm*(this.notesPerBeat/4)}</b></li>
+          </ul>
+        </div>
         {progresses}
+        <div>{(this.state.count - 1) % this.beatsInBar + 1}</div>
         <StartButton
           onClick={() => click()}
           counting={this.state.counting} />
@@ -101,7 +145,9 @@ class Metronome extends React.Component {
 
 class Progress extends React.Component {
   render() {
-    return <div className={'progress ' + (this.props.complete ? 'complete' : '')}></div>
+    return <div className={'progress ' + (this.props.complete ? 'complete' : '')}>
+       {this.props.number}
+    </div>
   }
 }
 
@@ -119,4 +165,4 @@ class StartButton extends React.Component {
   }
 }
 
-ReactDOM.render(<Metronome bpm={document.getElementById('bpm').value}/>, document.getElementById('root'));
+ReactDOM.render(<Metronome scale={JSON.parse(document.getElementById('scale').value)} bpm={document.getElementById('bpm').value}/>, document.getElementById('root'));
